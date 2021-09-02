bc85857b8249cd0fe6a188a994f6ed9db59e7d08
zeroSms <87300355+zeroSms@users.noreply.github.com>
2021-09-02 17:13:03 +0900

    eSenseアドレス自動取得

--- add_data.py
+++ add_data.py
@@ -12,31 +12,40 @@ import numpy as np
 import time
 import stop
 
+# ============================ 変数宣言部 ============================== #
 # ウィンドウ単位の処理用定数
-N = 20              # サンプル数
-OVERLAP = 50        # オーバーラップ率 [%]
 T = 10              # サンプリング周期 [Hz]
-byte_sample = 0
+OVERLAP = 50        # オーバーラップ率 [%]
+N = T * 2           # サンプル数
+byte_sample = bytearray([0x53, 0x03, 0x02, 0x01, 0x00])     # UUID7　書き込み用バイト（サンプリング開始）
 
 sec = 0             # 秒数
 data_queue = []     # 保存用変数
-
-# async def run():
-#     devices = await discover()
-#     for d in devices:
-#         print(d)
-#
-# loop = asyncio.get_event_loop()
-# loop.run_until_complete(run())
 
 # eSENSE キャラクタリスティックUUID
 UUID7 = "0000ff07-0000-1000-8000-00805f9b34fb"  # サンプリング開始/終了 (R/W)
 UUID8 = "0000ff08-0000-1000-8000-00805f9b34fb"  # データ取得 (R)
 UUIDE = "0000ff0e-0000-1000-8000-00805f9b34fb"  # フルスケールレンジ・ローパスフィルタの取得/変更 (R/W)
 
-# def sampling_byte(T):
-#     if T = 10:
-#         byte_sample = bytearray([0x53, 0x0d, 0x02, 0x01, 0x0a])
+
+# ============================ eSenseの処理 ============================== #
+# UUID7　書き込み用バイト（サンプリング開始）の周波数加算
+def sampling_byte():
+    byte_sample[1] += T
+    byte_sample[4] += T
+    return byte_sample
+
+# eSenseのアドレスを取得
+async def search_eSense():
+    global eSense_address
+    eSense_flg = True
+    while eSense_flg:
+        devices = await discover()
+        for d in devices:
+            if 'eSense' in str(d):
+                eSense_flg = False
+                print(d)
+                eSense_address = str(d).rsplit(':', 1)
 
 # センサの設定を行うクラス
 class Sensor:
@@ -64,8 +73,8 @@ class Sensor:
             x = await client.is_connected()
             print("Connected: {0}".format(x))
 
-            # サンプリング開始 20Hz
-            await client.write_gatt_char(UUID7, bytearray([0x53, 0x17, 0x02, 0x01, 0x14]), response=True)
+            # サンプリング開始
+            await client.write_gatt_char(UUID7, sampling_byte(), response=True)
 
             await client.start_notify(UUID8, Sensor.callback)
 
@@ -82,15 +91,15 @@ class Sensor:
         while stop.stop_flg:
             # キュー内のデータ数がサンプル数を超えたら作動
             if len(data_queue) > N:
-                notdup = int(N * (1 - OVERLAP / 100))  # 重複しない部分の個数
-                if notdup < 1:
-                    notdup = 1  # NotDupが0だと初期値の無限ループになる
+                not_dup = int(N * (1 - OVERLAP / 100))  # 重複しない部分の個数
+                if not_dup < 1:
+                    not_dup = 1
 
                 # サンプル数（N）分のデータを格納するリスト（window）の作成
-                for _ in range(notdup):
+                for _ in range(not_dup):
                     # 重複しない部分のデータはキューから削除
                     self.window.append(data_queue.pop(0))
-                for i in range(N - notdup):
+                for i in range(N - not_dup):
                     self.window.append(data_queue[i])
 
                 if self.window != []:
@@ -98,15 +107,15 @@ class Sensor:
                     self.window = []    # ウィンドウをリセット
                     return w
 
+
+# ============================ データ取得スレッド ============================== #
 def AddData():
     filename = "data_files/others/value_list" + input("file_number 入力：") + ".csv"
-    print(filename)
-    address = (
-        # discovery.pyでみつけたtoio Core Cubeのデバイスアドレスをここにセットする
-        "00:04:79:00:0D:00"  # Windows か Linux のときは16進6バイトのデバイスアドレスを指定
-        # if platform.system() != "Darwin"
-        # else "243E23AE-4A99-406C-B317-18F1BD7B4CBE"  # macOSのときはmacOSのつけるUUID
-    )
+    print("ファイル名：" + filename)
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(search_eSense())
+    address = eSense_address[0]
     loop = asyncio.get_event_loop()
     loop.run_until_complete(Sensor(address, loop).ReadSensor())
 
