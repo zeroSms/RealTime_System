bb96645906fbe8f64cdfe5b7867bf2923af7c586
zeroSms <s224461x@st.go.tuat.ac.jp>
2022-10-06 12:55:26 +0900

    2022/10/6

--- Execution_folder/server/server.js
+++ Execution_folder/server/server.js
@@ -6,13 +6,13 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-// // Labo PC
-// server.listen(3000, '192.168.2.111', () => {
-//   console.log('run');
-// });
-
-
-// note PC
-server.listen(3000, '192.168.2.19', () => {
+// Labo PC
+server.listen(3001, '192.168.2.111', () => {
   console.log('run');
 });
+
+
+// // note PC
+// server.listen(3001, '192.168.2.19', () => {
+//   console.log('run');
+// });
